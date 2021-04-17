df0498e3197bda316ea69715ea6512da9a49c3fd
viresh241196 <vireshrawool007@gmail.com>
2021-04-17 17:40:54 +0530

    added menthod to save the input entry

--- js/address_form.js
+++ js/address_form.js
@@ -1,46 +1,81 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    const name = document.querySelector("#fullname");
-    const textError = document.querySelector(".text-error");
-    name.addEventListener("input", function () {
-      if (name.value.length == 0) {
-        textError.textContent = "";
-        return;
-      }
-      try {
-        (new AddressBookData()).name = name.value;
-        textError.textContent = "";
-      } catch (e) {
-        textError.textContent = e;
-      }
-    });
+  const name = document.querySelector("#fullname");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      new AddressBookData().name = name.value;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
 });
 
-const address = document.querySelector('#address');
-const addressError = document.querySelector('.address-error')
-address.addEventListener('input', function(){
-    if(address.value.length==0){
-        addressError.textContent = "";
-        return;
-    }
-    try{
-        (new AddressBookData()).address = address.value;
-        addressError.textContent = "";
-    }catch(e){
-        addressError.textContent = e;
-    }
+const address = document.querySelector("#address");
+const addressError = document.querySelector(".address-error");
+address.addEventListener("input", function () {
+  if (address.value.length == 0) {
+    addressError.textContent = "";
+    return;
+  }
+  try {
+    new AddressBookData().address = address.value;
+    addressError.textContent = "";
+  } catch (e) {
+    addressError.textContent = e;
+  }
 });
 
-const phone = document.querySelector('#phoneNumber');
-const phoneError = document.querySelector('.phone-error')
-phone.addEventListener('input', function(){
-    if(phone.value.length==0){
-        phoneError.textContent = "";
-        return;
-    }
-    try{
-        (new AddressBookData()).phoneNumber = phone.value;
-        phoneError.textContent = "";
-    }catch(e){
-        phoneError.textContent = e;
-    }
+const phone = document.querySelector("#phoneNumber");
+const phoneError = document.querySelector(".phone-error");
+phone.addEventListener("input", function () {
+  if (phone.value.length == 0) {
+    phoneError.textContent = "";
+    return;
+  }
+  try {
+    new AddressBookData().phoneNumber = phone.value;
+    phoneError.textContent = "";
+  } catch (e) {
+    phoneError.textContent = e;
+  }
 });
+
+const save = () => {
+  try {
+    let addressBookData = createAddressBook();
+    console.log(addressBookData);
+    // createAndUpdateStorage(addressBookData);
+  } catch (e) {
+    return;
+  }
+};
+
+
+const createAddressBook = () => {
+  let addressBookData = new AddressBookData();
+  try {
+    addressBookData.name = getInputValueById("#fullname");
+    console.log("working");
+  } catch (e) {
+    setTextValue(".text-error", e);
+    console.log("not working");
+    throw e;
+  }
+  addressBookData.address = getInputValueById("#address");
+  addressBookData.city = getInputValueById("#city");
+  addressBookData.state = getInputValueById("#state");
+  addressBookData.zip = getInputValueById("#zip");
+  addressBookData.phoneNumber = getInputValueById("#phoneNumber");
+  alert(addressBookData.toString());
+  return addressBookData;
+};
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+  };
